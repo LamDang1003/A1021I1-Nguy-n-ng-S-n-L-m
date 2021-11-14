4e6047d6cc75af0f35e33ba58584fafa2f7b7f17
LamDang1003 <106180149@sv1.dut.udn.vn>
2021-11-15 00:08:00 +0700

    push bt

---
+++ 7. Biến, kiểu dữ liệu và toán tử/BAI_TAP/[Bài tập] Biến, kiểu dữ liệu và toán tử/TINH_DIEM_TB.js
@@ -0,0 +1,5 @@
+let physics = parseInt(prompt("physics points"))
+let chemistry = parseInt(prompt("chemistry points"))
+let biology = parseInt(prompt("biology points"))
+let GPA = (physics + chemistry + biology)/3
+document.write("Điểm trung bình GPA= ", +GPA)
